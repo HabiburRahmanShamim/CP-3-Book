23ec8b872916129f4dbdd256290b7315407a134e
Shamim Ahmed <shamimmahmed04@gmail.com>
2019-09-16 21:46:46 +0600

    Create 10141	Request for Proposal.cpp

---
+++ 1.3/Easy/10141	Request for Proposal.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n, p, caso = 0;
+    while(scanf("%d%d", &n, &p))
+    {
+        if(n == 0)break;
+        string p_s, brand, s;
+        getchar();
+        for(int i = 0; i < n; i++)getline(cin, p_s);
+        float d, f_ans = INT_MAX;
+        int r,  cur_ratio = 0;
+        for(int i = 0; i < p; i++)
+        {
+            getline(cin, s);
+            scanf("%f%d\n", &d, &r);
+            for(int j = 0; j < r; j++)getline(cin, p_s);
+            if(r == cur_ratio)
+            {
+                if(d < f_ans)
+                {
+                    f_ans = d;
+                    brand = s;
+                }
+            }
+            else if(r > cur_ratio)
+            {
+                f_ans = d;
+                brand = s;
+                cur_ratio = r;
+            }
+        }
+        if(caso != 0)printf("\n");
+        printf("RFP #%d\n", ++caso);
+        cout << brand << endl;
+    }
+    return 0;
+}
